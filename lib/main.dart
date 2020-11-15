e77079cbcc5abecb1fd3e49c7b920dd290d1bc7e
James Ho <jamesho19@gmail.com>
2020-11-15 22:46:26 +0800

    refactoring, the playSound into a funcation with parameter of file name.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,9 +9,79 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(children: <Widget>[
+              Expanded(
+                child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.red),
+                    onPressed: () {
+                      playSound(1);
+                    }),
+              ),
+              Expanded(
+                child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.orange)),
+                    onPressed: () {
+                      playSound(2);
+                    }),
+              ),
+              Expanded(
+                child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.yellow)),
+                    onPressed: () {
+                      playSound(3);
+                    }),
+              ),
+              Expanded(
+                child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green)),
+                    onPressed: () {
+                      playSound(4);
+                    }),
+              ),
+              Expanded(
+                child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue)),
+                    onPressed: () {
+                      playSound(5);
+                    }),
+              ),
+              Expanded(
+                child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.indigo)),
+                    onPressed: () {
+                      playSound(6);
+                    }),
+              ),
+              Expanded(
+                child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.purple)),
+                    onPressed: () {
+                      playSound(7);
+                    }),
+              ),
+            ]),
+          ),
         ),
       ),
     );
+  }
+
+  void playSound(int i) {
+    final player = AudioCache();
+    player.play('note$i.wav');
+    print('$i Clicked');
   }
 }
