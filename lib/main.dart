25a360ff6a37bf3715f6d6db1740abb1216d718f
James Ho <jamesho19@gmail.com>
2020-11-16 00:10:15 +0800

    refactor the expended TextButton into function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,45 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Expanded createKeys(int ii, Color colors) {
+    return Expanded(
+      child: TextButton(
+          style: TextButton.styleFrom(backgroundColor: colors),
+          onPressed: () {
+            playSound(i: ii);
+          }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
-            child: Column(children: <Widget>[
-              Expanded(
-                child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.red),
-                    onPressed: () {
-                      playSound(1);
-                    }),
-              ),
-              Expanded(
-                child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.orange)),
-                    onPressed: () {
-                      playSound(2);
-                    }),
-              ),
-              Expanded(
-                child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.yellow)),
-                    onPressed: () {
-                      playSound(3);
-                    }),
-              ),
-              Expanded(
-                child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.green)),
-                    onPressed: () {
-                      playSound(4);
-                    }),
-              ),
-              Expanded(
-                child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue)),
-                    onPressed: () {
-                      playSound(5);
-                    }),
-              ),
-              Expanded(
-                child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.indigo)),
-                    onPressed: () {
-                      playSound(6);
-                    }),
-              ),
-              Expanded(
-                child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.purple)),
-                    onPressed: () {
-                      playSound(7);
-                    }),
-              ),
-            ]),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  createKeys(1, Colors.red),
+                  createKeys(2, Colors.orange),
+                  createKeys(3, Colors.yellow),
+                  createKeys(4, Colors.green),
+                  createKeys(5, Colors.blue),
+                  createKeys(6, Colors.indigo),
+                  createKeys(7, Colors.purple),
+                ]),
           ),
         ),
       ),
     );
   }
 
-  void playSound(int i) {
+  void playSound({int i}) {
     final player = AudioCache();
     player.play('note$i.wav');
     print('$i Clicked');
